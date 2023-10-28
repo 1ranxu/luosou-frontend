f62917f0c8e37eb1f410de6b00846956d4479de0
1ranxu <263989312@qq.com>
2023-10-28 10:44:08 +0800

    add 聚合搜索

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -13,7 +13,7 @@
         <PostListPage :postlist="postList" />
       </a-tab-pane>
       <a-tab-pane key="picture" tab="图片">
-        <PictureListPage />
+        <PictureListPage :picturelist="pictureList" />
       </a-tab-pane>
       <a-tab-pane key="user" tab="用户">
         <UsertListPage :userlist="userList" />
@@ -31,27 +31,54 @@ import MyDivider from "@/pages/MyDivider.vue";
 import { useRoute, useRouter } from "vue-router";
 import myAxios from "@/plugins/myAxios";
 
-myAxios.post("/post/list/page/vo", {}).then((res: any) => {
-  postList.value = res.records;
-});
-
-myAxios.post("/user/list/page/vo", {}).then((res: any) => {
-  userList.value = res.records;
-});
-
-const router = useRouter();
-
-const route = useRoute();
-
+const postList = ref([]);
+const userList = ref([]);
+const pictureList = ref([]);
 const initSearchParams = {
   text: "",
   pageSize: 10,
   pageNum: 1,
 };
+const router = useRouter();
+const route = useRoute();
+
 const searchParams = ref(initSearchParams);
 
-const postList = ref([]);
-const userList = ref([]);
+/*const loadDataV1 = (params: any) => {
+  const postQuery = {
+    ...params,
+    searchText: searchParams.value.text,
+  };
+  myAxios.post("/post/list/page/vo", postQuery).then((res: any) => {
+    postList.value = res.records;
+  });
+  const userQuery = {
+    ...params,
+    userName: searchParams.value.text,
+  };
+  myAxios.post("/user/list/page/vo", userQuery).then((res: any) => {
+    userList.value = res.records;
+  });
+  const pictureQuery = {
+    ...params,
+    searchText: searchParams.value.text,
+  };
+  myAxios.post("/picture/list/page/vo", pictureQuery).then((res: any) => {
+    pictureList.value = res.records;
+  });
+};*/
+const loadData = (params: any) => {
+  const query = {
+    ...params,
+    searchText: searchParams.value.text,
+  };
+  myAxios.post("/search/all", query).then((res: any) => {
+    postList.value = res.postList;
+    userList.value = res.userList;
+    pictureList.value = res.pictureList;
+  });
+};
+loadData(initSearchParams);
 
 watchEffect(() => {
   searchParams.value = {
@@ -64,6 +91,7 @@ const onSearch = (value: string) => {
   router.push({
     query: searchParams.value,
   });
+  loadData(searchParams.value);
 };
 
 const onTabChange = (key: string) => {
--- src/pages/PictureListPage.vue
+++ src/pages/PictureListPage.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import luoying from "@/assets/luoying.png";
+import { defineProps, withDefaults } from "vue";
+
+interface Props {
+  picturelist: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  picturelist: () => [],
+});
+</script>
 
 <template>
-  <div>图片列表</div>
+  <div>
+    <a-list
+      :grid="{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3 }"
+      item-layout="horizontal"
+      :data-source="props.picturelist"
+    >
+      <template #renderItem="{ item }">
+        <a-list-item>
+          <a-card hoverable>
+            <template #cover>
+              <img alt="example" :src="item.url" />
+            </template>
+            <a-card-meta :title="item.title"> </a-card-meta>
+          </a-card>
+        </a-list-item>
+      </template>
+    </a-list>
+  </div>
 </template>
 
 <style scoped></style>
